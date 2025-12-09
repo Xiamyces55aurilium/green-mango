de53197fd9c4eb98f3543315ed3a8428acaef115
Xia <siyaswarnima55@gmail.com>
2025-12-09 19:06:54 +0530

    Refactor Random.java to comment out unused variables
    Commented out additional number generation code for clarity.

--- Random.java
+++ Random.java
@@ -3,7 +3,19 @@ public class Random{ //Make sure to name it with the same word which you'll use 
     public static void main(String[]args){ //It is java mandatory entry point,telling the JVM exactly where to start a standalone application
         Random random = new Random();// Because we need to generate a random number
         int number;
+        //int number1;(number1, number2, number3 are when you wish to print 3 random numbers)
+        //int number2;
+        //int number3;
         number = random.nextInt(1,10);//Here random numbers from 1-10 will be included and not 1-11
+        //number1 = random.nextInt(1,10);
+        //number2 = random.nextInt(1,10);
+        //number3 = random.nextInt(1,10);
+        
         System.out.print(number);//print the number
+        //System.out.println(number1); 
+        //System.out.println(number2);
+        //System.out.println(number3);
         }
+
 }
+
