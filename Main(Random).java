a4517e356eca7d5ba6ea05f04f51755cac1a8fb0
Xia <siyaswarnima55@gmail.com>
2025-12-09 19:08:24 +0530

    Update and rename Random.java to Main(Random).java

--- Main(Random).java
+++ Main(Random).java
@@ -1,5 +1,5 @@
 import java.util.Random;
-public class Random{ //Make sure to name it with the same word which you'll use for compiling.For eg- here I'll compile it using javac Random.java
+public class Main{ //Make sure to name it with the same word which you'll use for compiling.For eg- here I'll compile it using javac Main.java
     public static void main(String[]args){ //It is java mandatory entry point,telling the JVM exactly where to start a standalone application
         Random random = new Random();// Because we need to generate a random number
         int number;
@@ -18,4 +18,5 @@ public class Random{ //Make sure to name it with the same word which you'll use 
         }
 
 }
+
 
